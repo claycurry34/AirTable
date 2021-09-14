1d7d3c2933ad79aea1e5d540f4b389e1312400c4
Clay Curry (Work) <claycurry34@ou.edu>
2021-09-14 17:40:43 -0500

    added language selection functionality

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
-from typing import Dict
+from typing import Dict, List
 import requests
 from requests.structures import CaseInsensitiveDict
 import json
 
+map_is_initialized: bool = False
+character_map: Dict[str, Dict[str, any]] = None
 
-def load_database() -> Dict[str, Dict[str, str]]:
+
+def load_database() -> None:
     """
     Performs API request using AirTable api url and Clay's personal authorization key.
 
-    Creates a hash map (or a Dictionary, in Python) mapping uppercase letters to data associated with the letter
+    Returns a hash map (or a Dictionary, in Python) mapping uppercase letters to data associated with the letter
         {uppercase letters} -> { lowercase, English, Spanish, Igbo, Youruba, Kalabari}
     """
 
@@ -19,20 +22,46 @@ def load_database() -> Dict[str, Dict[str, str]]:
     # 2) Perform API request
     response = requests.get(api_url, headers=headers).text
     records = json.loads(response)['records']
-    # Parse API response
+    # 3) Parse API response
     outer_map = {}
     for row in records:
         column = row['fields']
         outer_map[column['Uppercase']] = {
-            "Lowercase": bool(column['Lowercase']),
-            "English": bool(column['English']),
-            "Spanish": bool(column['Spanish']),
-            "Igbo": bool(column['Igbo']),
-            "Yoruba": bool(column['Yoruba']),
-            "Kalabari": bool(column['Kalabari'])
+            "Lowercase": column['Lowercase'],
+            "English": column['English'] == "True",
+            "Spanish": column['Spanish'] == "True",
+            "Igbo": column['Igbo'] == "True",
+            "Yoruba": column['Yoruba'] == "True",
+            "Kalabari": column['Kalabari'] == "True"
         }
-    return outer_map
+
+    # 4) Cache response
+    global map_is_initialized
+    global character_map
+    map_is_initialized = True
+    character_map = outer_map
 
 
-letters = load_database()
-print(letters)
+def get_sorted_characters(requested_language: str) -> List[str]:
+    """
+    Accepts some language as an argument and
+
+    Returns a sorted array of characters from the requested language
+    """
+    global map_is_initialized
+    global character_map
+    if not map_is_initialized:
+        load_database()
+    requested_characters = []
+    for character in character_map.keys():
+        character_info = character_map.get(character)
+        if character_info.get(requested_language):
+            requested_characters.append(character)
+    return sorted(requested_characters)
+
+
+print(f"English: {get_sorted_characters('English')}")
+print(f"Spanish: {get_sorted_characters('Spanish')}")
+print(f"Igbo: {get_sorted_characters('Igbo')}")
+print(f"Yoruba: {get_sorted_characters('Yoruba')}")
+print(f"Kalabari: {get_sorted_characters('Kalabari')}")
