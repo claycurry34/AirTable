61be26476ec423d408905b47dac3feb80a92b960
Clay Curry (Home) <claycurry34@gmail.com>
2021-11-21 14:33:45 -0600

    removing separated files

--- ilearn.sys/Player.py
+++
@@ -1,9 +0,0 @@
-from typing import List
-from User import User
-from Game import Game
-
-
-class Player(User):
-    activeGame: Game
-    # the game the player is currently playing
-    active_statistics: List[str]
